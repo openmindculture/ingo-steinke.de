981bc6c2c946dbc89d3e373e887e538bbee81503
Ingo Steinke <ingosteinke@gmx.de>
2021-02-03 12:57:24 +0100

    seo: add redirects for image urls

--- .eleventy.js
+++ .eleventy.js
@@ -14,11 +14,16 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/apple-touch-icon.png");
   eleventyConfig.addPassthroughCopy("src/browserconfig.xml");
   eleventyConfig.addPassthroughCopy("src/site.webmanifest");
+  eleventyConfig.addPassthroughCopy("src/robots.txt");
   eleventyConfig.addPassthroughCopy("src/favicon.ico");
   eleventyConfig.addPassthroughCopy("src/favicon-16x16.png");
   eleventyConfig.addPassthroughCopy("src/favicon-32x32.png");
   eleventyConfig.addPassthroughCopy("src/mstile-150x150.png");
 
+  // redirect rules and domain configuration for netlify
+  eleventyConfig.addPassthroughCopy("src/netlify.toml");
+  // redirect rules for apache webhosting
+  eleventyConfig.addPassthroughCopy("src/.htaccess");
 
   eleventyConfig.addTransform("htmlmin", function(content, outputPath) {
     if( outputPath.endsWith(".html") ) {
