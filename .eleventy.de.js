b0b9998b5277a9efc7a3f71183047db630cf083b
Ingo Steinke <ingosteinke@gmx.de>
2022-01-10 12:42:16 +0100

    Linting, Formatting, Documentation (#71)
    * javascript formatting and documentation
    * setup eslint
    * add test tasks for linters

--- .eleventy.de.js
+++ .eleventy.de.js
@@ -1,29 +1,27 @@
-const htmlmin = require("html-minifier");
-module.exports = function(eleventyConfig) {
+const htmlmin = require('html-minifier');
+module.exports = function (eleventyConfig) {
   // explicit + fast way to copy certain files and folders
-  eleventyConfig.addPassthroughCopy("src/fonts");
-  eleventyConfig.addPassthroughCopy("src/img");
-  eleventyConfig.addPassthroughCopy("src/android-chrome-192x192.png");
-  eleventyConfig.addPassthroughCopy("src/apple-touch-icon.png");
-  eleventyConfig.addPassthroughCopy("src/browserconfig.xml");
-  eleventyConfig.addPassthroughCopy("src/site.webmanifest");
-  eleventyConfig.addPassthroughCopy("src/robots.txt");
-  eleventyConfig.addPassthroughCopy("src/favicon.ico");
-  eleventyConfig.addPassthroughCopy("src/favicon-16x16.png");
-  eleventyConfig.addPassthroughCopy("src/favicon-32x32.png");
-  eleventyConfig.addPassthroughCopy("src/mstile-150x150.png");
+  eleventyConfig.addPassthroughCopy('src/fonts');
+  eleventyConfig.addPassthroughCopy('src/img');
+  eleventyConfig.addPassthroughCopy('src/android-chrome-192x192.png');
+  eleventyConfig.addPassthroughCopy('src/apple-touch-icon.png');
+  eleventyConfig.addPassthroughCopy('src/browserconfig.xml');
+  eleventyConfig.addPassthroughCopy('src/site.webmanifest');
+  eleventyConfig.addPassthroughCopy('src/robots.txt');
+  eleventyConfig.addPassthroughCopy('src/favicon.ico');
+  eleventyConfig.addPassthroughCopy('src/favicon-16x16.png');
+  eleventyConfig.addPassthroughCopy('src/favicon-32x32.png');
+  eleventyConfig.addPassthroughCopy('src/mstile-150x150.png');
 
   // redirect rules and domain configuration for netlify
-  eleventyConfig.addPassthroughCopy("src/netlify.toml");
+  eleventyConfig.addPassthroughCopy('src/netlify.toml');
   // redirect rules for apache webhosting
-  eleventyConfig.addPassthroughCopy("src/.htaccess");
+  eleventyConfig.addPassthroughCopy('src/.htaccess');
 
-  eleventyConfig.addTransform("htmlmin", function(content, outputPath) {
-    if( outputPath.endsWith(".html") ) {
+  eleventyConfig.addTransform('htmlmin', function (content, outputPath) {
+    if (outputPath.endsWith('.html')) {
       let minified = htmlmin.minify(content, {
-        useShortDoctype: true,
-        removeComments: true,
-        collapseWhitespace: true
+        useShortDoctype: true, removeComments: true, collapseWhitespace: true
       });
       return minified;
     }
@@ -34,10 +32,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPairedShortcode("postcss", require("./utils/transform-css"));
   return {
     dir: {
-      input: "src",
-      data: "_data/de",
-      output: "dist"
-    },
-    templateFormats: ["liquid"]
+      input: 'src', data: '_data/de', output: 'dist'
+    }, templateFormats: ['liquid']
   }
 };
