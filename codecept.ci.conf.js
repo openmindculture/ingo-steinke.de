5d169b369cc24bad2d84f6fffe822729306503bc
Ingo Steinke <ingosteinke@gmx.de>
2022-09-02 16:12:20 +0200

    refactor codecept config, rebuild base screenshot

--- codecept.ci.conf.js
+++ codecept.ci.conf.js
@@ -1,12 +1,7 @@
-// in codecept.conf.js
-const { setWindowSize } = require('@codeceptjs/configure');
+const { setHeadlessWhen, setWindowSize } = require('@codeceptjs/configure');
+exports.config = require ('./codecept.common.conf.js');
+exports.config.helpers.Puppeteer.url = 'https://www.ingo-steinke.de';
 
-setWindowSize(1600, 1200);
-
-exports.config = {
-  helpers: {
-    Puppeteer: {
-      url: 'https://www.ingo-steinke.de',
-    }
-  }
-}
+// turn on headless mode when running with HEADLESS=true environment variable
+// export HEADLESS=true && npx codeceptjs run
+setHeadlessWhen(process.env.HEADLESS);
--- codecept.conf.js
+++ codecept.conf.js
@@ -1,48 +1,7 @@
 const { setHeadlessWhen, setWindowSize } = require('@codeceptjs/configure');
+exports.config = require ('./codecept.common.conf.js');
+exports.config.helpers.Puppeteer.url = 'http://localhost:1974'; // or 'https://www.ingo-steinke.de';
 
 // turn on headless mode when running with HEADLESS=true environment variable
 // export HEADLESS=true && npx codeceptjs run
 setHeadlessWhen(process.env.HEADLESS);
-
-setWindowSize(1600, 1200);
-
-exports.config = {
-  tests: './codecept/*_test.js',
-  output: './codecept/output',
-  helpers: {
-    Puppeteer: {
-      url: 'http://localhost:1974',
-      show: true,
-      windowSize: '1200x900'
-    },
-    "ResembleHelper" : {
-      "require": "codeceptjs-resemblehelper",
-      "screenshotFolder" : "./codecept/output/",
-      "baseFolder": "./codecept/screenshots/base/",
-      "diffFolder": "./codecept/screenshots/diff/"
-    }
-  },
-  include: {
-    I: './codecept/steps_file.js'
-  },
-  bootstrap: null,
-  mocha: {},
-  name: 'ingo-steinke.de',
-  plugins: {
-    testomatio: {
-      enabled: true,
-      require: '@testomatio/reporter/lib/adapter/codecept',
-      apiKey: process.env.TESTOMATIO,
-    },
-    pauseOnFail: {},
-    retryFailedStep: {
-      enabled: true
-    },
-    tryTo: {
-      enabled: true
-    },
-    screenshotOnFail: {
-      enabled: true
-    }
-  }
-}
