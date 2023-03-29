1f3ab272b1ef54a6cd13a2ea30c359def6aa7ba2
Ingo Steinke <ingosteinke@gmx.de>
2023-03-29 20:24:29 +0200

    testing work in progress

--- codecept/homepage_test.js
+++ codecept/homepage_test.js
@@ -13,14 +13,20 @@ Scenario('Test website content and navigation', async({ I }) => {
     prepareBaseImage: false // true: overwrite base images, false: compare
   });
 
-  
+  // If there is a mobile burger menu button, we must click it first.
+  // As conditionals are not allowed in test scenarios,
+  // we could find a more generic click path that applies to any device variation.
+  // Workaround: define an alternative in Xpath even if that means a redundant click on 'Projekte':
+  I.seeElement('//a[contains("class", "menu-toggle") or contains(., "Inhalt") or contains(., "Projekte")]');
 
   // I can "see" something below the fold (implicitly scrolling it into view?)
   // but then, how can we test links actually scrolling down to another one-pager section?
-  I.seeElement('//a[contains(., "Projekte")]');
+  I.see('Projekte');
 
   // By default, CodeceptJS tries to find the button or link with the exact text on it
-  I.click('Projekte');
+  // but we might need XPath here as well to make it work inside the mobile menu, but why?
+  // the following still fails with Node is either not clickable or not an HTMLElement
+  I.click('//a[contains(., "Projekte") or contains(href, "#projects")]');
   I.see('Eine Auswahl meiner bisherigen Projekte');
 
   I.seeElement('//a[contains(., "Datenschutz")]');
