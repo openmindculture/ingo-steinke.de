bf0126cc2dc88a4ee1102741b448ab7eb74c0ce0
Ingo Steinke <ingosteinke@gmx.de>
2024-03-15 11:50:27 +0100

    add matomo tracking #212

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -80,4 +80,18 @@ window.addEventListener('DOMContentLoaded', () => {
       decorationElement.style.setProperty(randomProperties[i], Math.random());}
   }
 
+  var _paq = window._paq = window._paq || [];
+  // tracker methods like "setCustomDimension" should be called before "trackPageView"
+  _paq.push(["setDomains", ["*.ingo-steinke.com", "*.ingo-steinke.de"]]);
+  _paq.push(["enableCrossDomainLinking"]);
+  _paq.push(['trackPageView']);
+  // _paq.push(['enableLinkTracking']); // disable link tracking
+  (function() {
+    var u="//www.ingo-steinke.de/matomo/";
+    _paq.push(['setTrackerUrl', u+'matomo.php']);
+    _paq.push(['setSiteId', '1']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+  })();
+
 });
