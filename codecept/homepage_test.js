2a5fe217e6af5753cf3307b1eb6ae17ba98c649c
Ingo Steinke <ingosteinke@gmx.de>
2024-04-16 12:37:07 +0200

    update screenshot tests #225

--- codecept/homepage_test.js
+++ codecept/homepage_test.js
@@ -4,7 +4,7 @@ Scenario('Test website content and navigation', ({ I }) => {
 
   // Set prepareScreenshotBaseImages to true to regenerate and save all screenshots,
   // set prepareScreenshotBaseImages to compare upcoming tests to saved screenshots:
-  let prepareScreenshotBaseImages = true;
+  let prepareScreenshotBaseImages = false;
 
   I.amOnPage('/');
   I.see('Ingo Steinke', 'h1');
@@ -23,14 +23,12 @@ Scenario('Test website content and navigation', ({ I }) => {
   // By default, CodeceptJS tries to find the button or link with the exact text on it
   I.click('Datenschutz');
   I.see('Umfang der Verarbeitung personenbezogener Daten');
-
-  // We can ensure NOT to see something:
-  I.dontSeeElement('.error');
-
-  I.click('Kontakt');
   I.see('Schreib mir eine Nachricht');
   I.see('Nachricht absenden');
   I.see('buche einen Termin');
+
+  // We can ensure NOT to see something:
+  I.dontSeeElement('.error');
 
   // TODO local links should not point to fixed production urls!
   // TODO how to test language page linking locally?
@@ -38,34 +36,23 @@ Scenario('Test website content and navigation', ({ I }) => {
   // but as a pragmatic workaround, we could use a classic dead-link-checker
   // and check for search console errors after deployment
 
-  I.click('Nachhaltigkeit');
+  I.amOnPage('/webseiten-klimafreundlich-barrierefrei-optimieren/index.html');
   I.see('Webseiten klimafreundlich und barrierefrei optimieren lassen');
-  I.see('Beratungsgespräch vereinbaren');
   I.saveScreenshot('Sustainability_DE_Screenshot.png');
-  I.seeVisualDiff('Sustainability-Optimize_DE_Screenshot.png', {
+  I.seeVisualDiff('Sustainability_DE_Screenshot.png', {
     tolerance: 2,
     prepareBaseImage: prepareScreenshotBaseImages
   });
 
   I.amOnPage('/wordpress-website-nachhaltig-optimieren-lassen/index.html');
   I.see('Individuell optimierte WordPress-Themes');
-  I.see('Kontakt aufnehmen');
   I.saveScreenshot('WordPress_DE_Screenshot.png');
   I.seeVisualDiff('WordPress_DE_Screenshot.png', {
     tolerance: 2,
     prepareBaseImage: prepareScreenshotBaseImages
   });
 
-  I.amOnPage('/leistungen/webentwickler-berlin-freelancer.html');
-  I.see('jetzt Kontakt aufnehmen');
-  I.saveScreenshot('Freelancer_Berlin_DE_Screenshot.png');
-  I.seeVisualDiff('Freelancer_Berlin_DE_Screenshot.png', {
-    tolerance: 2,
-    prepareBaseImage: prepareScreenshotBaseImages
-  });
-
-  I.amOnPage('/leistungen/webentwickler-berlin-freelancer.html');
-  I.see('jetzt Kontakt aufnehmen');
+  I.amOnPage('/zertifizierter-shopware-6-frontend-webentwickler/index.html');
   I.saveScreenshot('Shopware_DE_Screenshot.png');
   I.seeVisualDiff('Shopware_DE_Screenshot.png', {
     tolerance: 2,
@@ -73,60 +60,34 @@ Scenario('Test website content and navigation', ({ I }) => {
   });
 
 
-
-  I.amOnPage('/wordpress-website-nachhaltig-optimieren-lassen/index.html');
-  I.see('Individuell optimierte WordPress-Themes');
-  I.see('Kontakt aufnehmen');
-  I.saveScreenshot('WordPress_DE_Screenshot.png');
-  I.seeVisualDiff('WordPress_DE_Screenshot.png', {
+  I.amOnPage('/leistungen/webentwickler-berlin-freelancer.html');
+  I.saveScreenshot('Freelancer_Berlin_DE_Screenshot.png');
+  I.seeVisualDiff('Freelancer_Berlin_DE_Screenshot.png', {
     tolerance: 2,
     prepareBaseImage: prepareScreenshotBaseImages
   });
 
-  I.click('deutsch');
+  I.amOnPage('/leistungen/webentwicklung.html');
   I.see("Wir bauen deine Website!");
-  I.see('Beratungstermin vereinbaren');
   I.saveScreenshot('Services-Web_DE_Screenshot.png');
   I.seeVisualDiff('Services-Web_DE_Screenshot.png', {
     tolerance: 2,
     prepareBaseImage: prepareScreenshotBaseImages
   });
 
-  I.click('Web-Check');
-  I.see("Website überprüfen");
-  I.see('Beratungstermin vereinbaren');
+  I.amOnPage('/leistungen/website-check.html');
+  I.see("Lass deine Website überprüfen!");
   I.saveScreenshot('Services-Check_DE_Screenshot.png');
   I.seeVisualDiff('Services-Check_DE_Screenshot.png', {
     tolerance: 2,
     prepareBaseImage: prepareScreenshotBaseImages
   });
 
-  I.click('English');
-  I.see('verify your website');
-  I.see('schedule an appointment');
-  I.saveScreenshot('Services-Check_EN_Screenshot.png');
-  I.seeVisualDiff('Services-Check_EN_Screenshot.png', {
+  I.amOnPage('/leistungen/optimierung.html');
+  I.see("Websites erweitern, überarbeiten und optimieren lassen");
+  I.saveScreenshot('Services-Optimization_DE_Screenshot.png');
+  I.seeVisualDiff('Services-Optimization_DE_Screenshot.png', {
     tolerance: 2,
     prepareBaseImage: prepareScreenshotBaseImages
   });
-
-  I.click('optimize');
-  I.see('optimize your website');
-  I.see('schedule an appointment');
-  I.saveScreenshot('Services-Optimize_EN_Screenshot.png');
-  I.seeVisualDiff('Services-Optimize_EN_Screenshot.png', {
-    tolerance: 2,
-    prepareBaseImage: prepareScreenshotBaseImages
-  });
-
-  I.click('deutsch');
-  I.see('erweitern, überarbeiten und optimieren');
-  I.see('Beratungstermin vereinbaren');
-  I.saveScreenshot('Services-Optimize_DE_Screenshot.png');
-  I.seeVisualDiff('Services-Optimize_DE_Screenshot.png', {
-    tolerance: 2,
-    prepareBaseImage: prepareScreenshotBaseImages
-  });
-
-
 });
