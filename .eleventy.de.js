e64806f3e05887e63d5ec78b106a388389e2e7f6
Ingo Steinke <ingosteinke@ingosteinke.de>
2025-12-04 18:50:22 +0100

    update favicon icons

--- .eleventy.de.js
+++ .eleventy.de.js
@@ -15,14 +15,11 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('src/img');
   eleventyConfig.addPassthroughCopy('src/vcard');
   eleventyConfig.addPassthroughCopy('src/android-chrome-192x192.png');
+  eleventyConfig.addPassthroughCopy('src/android-chrome-512x512.png');
   eleventyConfig.addPassthroughCopy('src/apple-touch-icon.png');
-  eleventyConfig.addPassthroughCopy('src/browserconfig.xml');
   eleventyConfig.addPassthroughCopy('src/site.webmanifest');
   eleventyConfig.addPassthroughCopy('src/robots.txt');
   eleventyConfig.addPassthroughCopy('src/favicon.ico');
-  eleventyConfig.addPassthroughCopy('src/favicon-16x16.png');
-  eleventyConfig.addPassthroughCopy('src/favicon-32x32.png');
-  eleventyConfig.addPassthroughCopy('src/mstile-150x150.png');
   eleventyConfig.addPassthroughCopy('src/2022-4-19-www.ingo-steinke.de-1793046390-report.pdf');
   eleventyConfig.addPassthroughCopy('src/2022-5-27-www.ingo-steinke.com-1503125752-report.pdf');
   eleventyConfig.addPassthroughCopy('src/Shopware-6-Certified-Developer-Ingo-Steinke.pdf');
